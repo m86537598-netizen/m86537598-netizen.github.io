55fd463ab908cbc975e0b578ceffa4b357af5bf5
Pet-story <m86537598@gmail.com>
2026-01-02 03:51:02 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,19 @@
+/* THE DATABASE
+   Admin: BUBBLDEDS
+   Password: (Only used for local verification, real security is YOU editing this file)
+*/
+
+const DB = {
+    users: [
+        // The Admin Account
+        { username: "BUBBLDEDS", password: "ERG98UESG9", isBanned: false }
+    ],
+    pages: [
+        {
+            title: "Main Page",
+            content: "Welcome to the '''Dandy World Skins Wiki'''!\n\nThis is the community wiki. \n\n[[Request a Page]] to contribute!\n\n{{Infobox|Dandy World|https://placehold.co/150}}",
+            comments: []
+        }
+    ],
+    requests: []
+};
